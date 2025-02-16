8645c0105fd73ab6169cb622e22a096dbcb0ae08
Gustavo Robson <gustavo.silva510@fatec.sp.gov.br>
2025-02-16 19:48:01 -0300

    links-codes

--- landingPage/index.js
+++ landingPage/index.js
@@ -1,6 +1,18 @@
 function calculadora(){
     window.location.href = "/calculadora/calculadora.html";
 }
+function codeCalculadora(){
+    window.open("https://github.com/GustavoRobs-11/mini-projetos/tree/main/calculadora");
+}
 function mario(){
     window.location.href = "/jogo-Mario/jogoMario.html"
+}
+function codeMario(){
+    window.open("https://github.com/GustavoRobs-11/mini-projetos/tree/main/jogo-Mario");
+}
+function mind(){
+    window.open("https://mind-your-359d.vercel.app/");
+}
+function codeMind(){
+    window.open("https://github.com/Mind-your/Mind_Entrega_1_Semestre");
 }
